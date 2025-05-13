fb0903f6895852e2e04649ade2b5aa9404b36574
Rhoda-26 <rhodarimisola@gmail.com>
2025-05-13 17:48:28 +0100

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,51 @@
+#print("Hello World")
+#Day 1 exercise 1
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+print("Hello world\nHello world\nHello world")
+
+print("Hello" + "Rhoda ")
+print("Hello + Rhoda")
+print("Hello" + " " + "Rhoda")
+
+#Day1 exercise 2
+print("Day 1 - String Manipulation\nString concatenation")
+print("String concatenation is done with "+" sign")
+print('e.g. print("Hello " + "world")')
+print("new line is created with backlash and n")
+
+
+#input function
+input("What's your name?")
+print("Hello" + input("What's your name?"))
+
+
+#Day1 exercise 3
+#program to calculate length of a string
+print(len(input("What's your name?")))
+
+
+
+name = input("What's your name?")
+print(name)
+
+name = "Rimisola"
+print(name)
+
+name = input("What's your name?")
+length = len(name)
+print(length)
+
+
+#Day 1 Exercise 4
+a = input("a:")
+b = input("b:")
+
+print("a = " + a)
+print("b = " + b)
+
+#Variables naming
+name =input("What's the name of your bestfriend?")
+print(name)
